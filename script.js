da707c85383115db1af68bcdccb3781382c1d801
yulopadua <yulo.padua@gmail.com>
2022-05-03 16:57:57 +0800

    Add functions to play single and five rounds

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-let computerChoice = ['Rock', 'Paper', 'Scissors'];
-
-//function to randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
+//function to randomly return either rock, paper or scissors
 function computerPlay() {
-    let random = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+    let randomNumber = Math.floor(Math.random() * 3)
 
-    return random;
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else if (randomNumber === 2) {
+        return "scissors";
+    }
 }
+
+//function to play single round
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") || 
+        (playerSelection === "scissors" && computerSelection === "paper")) {
+        return "You win!";
+    } else if (
+        (computerSelection === "rock" && playerSelection === "scissors") ||
+        (computerSelection === "paper" && playerSelection === "rock") ||
+        (computerSelection === "scissors" && playerSelection === "paper")) {
+        return "You lose!";
+    }
+}
+
+//function to play five rounds
+function game() {
+    let playerScore = "";
+    let computerScore = "";
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Rock, Paper or Scissors?", "");
+        const computerSelection = computerPlay();
+        console.log(computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game();
